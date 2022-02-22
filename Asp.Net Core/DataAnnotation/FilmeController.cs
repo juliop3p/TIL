b414f955a74fad83f56259d08d4b4ab9aba2e12b
Julio Cesar <julio15.zn@gmail.com>
2022-02-21 22:59:44 -0300

    Data Annotations ASP.NET CORE

---
+++ Asp.Net Core/DataAnnotation/FilmeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataAnnotation
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        public FilmeController()
+        {
+        }
+    }
+}
